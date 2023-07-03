a4afee7560cb0457f425d2479d4ce5cb5d35dd67
Damien Tardy-Panis <damien.dev@tardypad.me>
2023-07-03 14:47:42 +0200

    setup: run pacstrap interactively
    So we can choose a non default dependency for packages that require one
    satisfied by multiple providers

--- setup/setup-1.sh
+++ setup/setup-1.sh
@@ -112,7 +112,7 @@ install_system_config() {
   done
   printf '\n'
 
-  pacstrap -K /mnt "system-config-${SYSTEM_CONFIG}" --overwrite '*'
+  pacstrap -K -i /mnt "system-config-${SYSTEM_CONFIG}" --overwrite '*'
 
   # we set the packages password on the end system while we can
   sed -i "s/{PASSWORD}/${PKG_PWD}/" /mnt/etc/pacman.d/chestnut
